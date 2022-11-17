bd8f5bcf59604237644391776a0cb6444ec6c066
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-11-17 06:23:37 +0200

    init commit

--- BackEnd/migrations/sqls/20221108224400-shelfs-table-up.sql
+++ BackEnd/migrations/sqls/20221108224400-shelfs-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE shelfs (
     id SERIAL PRIMARY KEY,
-    shelf_number INT UNIQUE NOT NULL,
+    shelf_number INT NOT NULL,
     shelf_name VARCHAR(100) UNIQUE,
     block_id integer REFERENCES blocks(id) NOT NULL,
     created_date TIMESTAMP NOT NULL,
