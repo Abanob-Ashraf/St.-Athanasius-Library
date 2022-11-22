d530f36e17619317c7fe9c4dc225e2400c21ff0c
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-11-22 04:59:18 +0200

    init commit

--- BackEnd/src/sql-queries/users.ts
+++ BackEnd/src/sql-queries/users.ts
@@ -1,15 +1,20 @@
+//create user
 export const createUser =
   'INSERT INTO users (first_name, last_name, email, password, admin_flag, created_date, updated_date) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, first_name, last_name, email, admin_flag, updated_date, created_date'
 
+//getAll Users
 export const getAllUsers =
   'SELECT id, first_name, last_name, email, admin_flag, created_date, updated_date FROM users'
 
+//getOne User
 export const getSingleUserById =
   'SELECT id, first_name, last_name, email, admin_flag, created_date, updated_date FROM users WHERE id=($1)'
 
+//Update User
 export const updateUser =
   'UPDATE users SET first_name=($2), last_name=($3), email=($4), password=($5), admin_flag=($6), updated_date=($7) WHERE id=($1) RETURNING id, first_name, last_name, email, admin_flag, updated_date, created_date'
 
+//Delete User
 export const dropFkey = 'ALTER TABLE Books DROP CONSTRAINT books_user_id_fkey'
 
 export const deleteUser =
@@ -20,6 +25,7 @@ export const updateBookAfterDeleteUser = 'UPDATE books SET user_id = NULL WHERE 
 export const addFkey =
   'ALTER TABLE Books ADD CONSTRAINT books_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)'
 
+//Sign in
 export const authenticateQ = 'SELECT password FROM users WHERE email=($1)'
 
 export const authenticateQ2 =
