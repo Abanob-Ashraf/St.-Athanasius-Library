b10ee188bd39b59b9447d72bb369c789759aeba1
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-12-02 12:34:11 +0200

    init commit

--- BackEnd/src/sql-queries/shelfs.ts
+++ BackEnd/src/sql-queries/shelfs.ts
@@ -3,7 +3,7 @@ export const CREATESHELF =
 
 export const GETMANYSHELFS = 'SELECT * FROM shelfs'
 
-export const GETONESHELF = 'SELECT * FROM shelfs WHERE id=($1);'
+export const GETONESHELF = 'SELECT * FROM shelfs WHERE id=($1)'
 
 export const UPDATESHELF =
   'UPDATE shelfs SET shelf_number=($2), shelf_name=($3), block_id=($4), updated_date=($5) WHERE id=($1) RETURNING *'
