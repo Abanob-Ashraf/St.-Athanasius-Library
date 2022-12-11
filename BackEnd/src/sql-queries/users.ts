4d596b4d9ac2c06783d7641e86f813963b809d5c
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-12-11 02:07:09 +0200

    init commit

--- BackEnd/src/sql-queries/users.ts
+++ BackEnd/src/sql-queries/users.ts
@@ -21,7 +21,7 @@ export const DELETEUSER =
   'UPDATE users SET user_status=($2), updated_date=($3) WHERE id=($1) RETURNING *'
 
 export const UPDATEBOOKAFTERDELETEUSER =
-  'UPDATE books SET user_id=($2), old_user=($1) WHERE user_id=($1)'
+  'UPDATE books SET currrent_user=($2), old_user=($1) WHERE currrent_user=($1)'
 
 //Sign in
 export const AUTHANTICATE = 'SELECT password FROM users WHERE email=($1)'
