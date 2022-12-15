ed71bf32b4906f024a0767c0e0acf629babedada
Abanob-Ashraf <abanobashraf74@gmail.com>
2022-12-15 17:52:34 +0200

    init commit

--- BackEnd/migrations/sqls/20221122115250-users-table-up.sql
+++ BackEnd/migrations/sqls/20221122115250-users-table-up.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     last_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(11),
+    phone_number VARCHAR(11) UNIQUE,
     admin_flag BOOLEAN NOT NULL,
     user_status VARCHAR(20) NOT NULL,
     created_date TIMESTAMP NOT NULL,
