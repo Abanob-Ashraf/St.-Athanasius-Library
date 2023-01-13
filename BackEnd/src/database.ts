2ab5c9012a3050623fc159a63f5c515a1502f760
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-01-13 02:08:14 +0200

    init commit

--- BackEnd/src/database.ts
+++ BackEnd/src/database.ts
@@ -4,23 +4,14 @@ import { Pool } from 'pg'
 
 dotenv.config()
 
-// eslint-disable-next-line prettier/prettier
-const { 
-  DB_HOST, 
-  DB_PORT, 
-  DB_NAME, 
-  DB_NAME_TEST, 
-  DB_USER, 
-  DB_PASSWORD, 
-  ENV 
-} = process.env
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, ENV } = process.env
 
 console.log(ENV)
 console.log(`Database is connected at prot:${DB_PORT}`)
 
 const Client = new Pool({
   host: DB_HOST,
-  database: ENV === 'dev' ? DB_NAME : DB_NAME_TEST,
+  database: DB_NAME,
   user: DB_USER,
   password: DB_PASSWORD,
   port: parseInt(DB_PORT as string)
