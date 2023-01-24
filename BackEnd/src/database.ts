dfdb49f6e33c26e0797f3a4698b7629dcb25c528
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-01-24 18:06:24 +0200

    init commit

--- BackEnd/src/database.ts
+++ BackEnd/src/database.ts
@@ -5,7 +5,7 @@ import { Pool } from 'pg'
 dotenv.config()
 
 const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD, ENV } = process.env
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env
+const { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD } = process.env
 
 console.log(ENV)
 
@@ -14,7 +14,7 @@ const Client = new Pool({
   database: ENV === 'dev' ? DB_NAME : PGDATABASE,
   user: ENV === 'dev' ? DB_USER : PGUSER,
   password: ENV === 'dev' ? DB_PASSWORD : PGPASSWORD,
-  port: parseInt(DB_PORT as string),
+  port: ENV === 'dev' ? parseInt(DB_PORT as string) : parseInt(PGPORT as string),
   ssl: ENV === 'dev' ? false : true
 })
 
