dc60f5d4b495b636a5a0cf8b12244d84f8511609
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-02-01 01:50:40 +0200

    init commit

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -16,8 +16,8 @@ dotenv.config()
 // create an instance server
 const app: Application = express()
 
-const privateKey = fs.readFileSync('../server.key')
-const certificate = fs.readFileSync('../server.cert')
+const privateKey = fs.readFileSync('server.key')
+const certificate = fs.readFileSync('servr.cert')
 
 // HTTP request logger middleware
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
