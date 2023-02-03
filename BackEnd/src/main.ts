0d69b3f141ebccbfafda8481cb2bfc774c743ae1
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-02-03 12:06:19 +0200

    init commit

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -34,7 +34,7 @@ app.use(cors())
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 50, // Limit each IP to 50 requests per `window` (here, per 15 minutes)
+    max: 500, // Limit each IP to 50 requests per `window` (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     statusCode: 429, // StatusCode = 429 Too Many Requests
