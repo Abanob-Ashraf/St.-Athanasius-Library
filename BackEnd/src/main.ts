ec55d2864565f813d69820b11b7e5b5a394ea92f
Abanob-Ashraf <abanobashraf74@gmail.com>
2023-02-06 21:31:50 +0200

    init commit

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -50,11 +50,11 @@ app.use((_: Request, res: Response) => {
 
 // start express server
 const PORT = 3000
-https
-  .createServer({ key: privateKey, cert: certificate }, app)
-  .listen(process.env.PORT || PORT, () =>
-    // eslint-disable-next-line no-console
-    console.log(`Server is starting at prot:${PORT}`)
-  )
+// https
+//   .createServer({ key: privateKey, cert: certificate }, app)
+app.listen(process.env.PORT || PORT, () =>
+  // eslint-disable-next-line no-console
+  console.log(`Server is starting at prot:${PORT}`)
+)
 
 export default app
