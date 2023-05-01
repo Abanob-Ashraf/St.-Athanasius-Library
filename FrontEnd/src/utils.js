e60911076c9ada14cf76caba95206b6724ecf518
kerolos reda <114709499+MRxRE@users.noreply.github.com>
2023-05-02 02:17:57 +0300

    init commit

--- FrontEnd/src/utils.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom"
-
-export async function requirdAuth () {
-    const isLoggend = sessionStorage.getItem("user");
-    if (!isLoggend){
-        throw redirect("/")
-    }
-    return null;
-}
